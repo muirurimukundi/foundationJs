0b440ecfc9e2c78a8478a519f3c9099e01ad13ab
John Muiruri <themasalacollective@gmail.com>
2022-04-06 08:51:07 +0300

    Math.sqrt, template strings use

---
+++ foundationJs.js
@@ -0,0 +1,19 @@
+// For Practice and Enjoyment of Js Syntax and foundations
+
+// Print Hello World
+
+console.log("Hello World");
+
+// // Add two numbers 
+
+// var number1 = parseInt(prompt("Enter first number:"));
+// var number2 = parseInt(prompt("Enter second number"));
+
+// alert(`${number1} + ${number2} is equals ${number1+number2}`);
+
+
+// Square root of a number
+
+var userNumber = parseInt(prompt("Enter a number and we'll get it's square root:"));
+
+alert(`The square root of ${userNumber} is ${Math.sqrt(userNumber)}`);
